961a190fb45520b431f4c77ae2d95f39cad36cd2
pavelbraginskiy <pavelbraginskiy@hotmail.com>
2017-07-22 08:06:58 -0700

    Fixed whitespace mangling caused by GitHub editor.

--- src/cli/colorSupport.js
+++ src/cli/colorSupport.js
@@ -10,7 +10,7 @@ function getSupport() {
         // won't work. However, here we target Node.js 8 at minimum as it is an LTS
         // release, and Node.js 7 is not. Windows 10 build 10586 is the first Windows
         // release that supports 256 colors.
-	var os = require('os');
+        var os = require('os');
         const osRelease = os.release().split('.');
         if (
             Number(process.version.split('.')[0]) >= 8 &&
