70d1114d771e25e71e8a8f2633a62634053f6e15
Vihan <vihan+github@vihan.org>
2018-09-29 18:34:18 -0700

    Work on adjusting build process
    Signed-off-by: Vihan <vihan+github@vihan.org>

--- src/vsl/backend/llvm/watchers/Generic.js
+++ src/vsl/backend/llvm/watchers/Generic.js
@@ -12,30 +12,9 @@ export default class LLVMGeneric extends BackendWatcher {
     receive(node, tool, regen, context) {
         const backend = context.backend;
 
-        const asLValue = context.popValue(Key.LValueContext);
-
-        if (node.reference === null) {
-            throw new BackendError(
-                `Item ${node.value} has ambigious reference in this context.`,
-                node
-            );
-        } else {
-            // If ScopeTypeItem then we have metaclass
-            if (node.reference instanceof ScopeTypeItem) {
-                // This means we have a static class. We will return the
-                // @static.Goat { field1, field2, field3 } obj which is globally
-                // init'd.
-                const source = node.reference.source;
-                const newCtx = context.bare();
-                return regen(source.relativeName, source.parentNode, newCtx);
-            } else {
-                // Otherwise we're referring to variable
-                if (asLValue) {
-                    return node.reference.backendRef;
-                } else {
-                    return node.reference.backendRef.generate(context);
-                }
-            }
-        }
+        throw new BackendError(
+            `Generics not supported :(.`,
+            node
+        );
     }
 }
