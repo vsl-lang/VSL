1d18204136cd3905501dfd771f0f565ec86896a8
Vihan <vihan+github@vihan.org>
2019-01-28 21:31:05 -0800

    Fix issues in parser server
    Signed-off-by: Vihan <vihan+github@vihan.org>

--- src/cli/mode/parserServer.js
+++ src/cli/mode/parserServer.js
@@ -11,7 +11,10 @@ import net from 'net';
 import lz4 from 'lz4';
 
 export const DEFAULT_PORT = 8752;
+export const DEFAULT_TIMEOUT = 400;
 export const SERVER_PORT_KEY = 'VSL_SERVER_PORT_KEY';
+export const SERVER_TIMEOUT_KEY = 'VSL_SERVER_TIMEOUT_KEY';
+export const COLOR_KEY = 'VSL_COLOR_KEY';
 
 export default class ParserServer extends CompilerCLI {
     usage = "vsl parser-server [options] [port]"
@@ -22,6 +25,13 @@ export default class ParserServer extends CompilerCLI {
                 ["-h", "--help"          , "Displays this help message",             { run: _ => _.help() }],
                 ["--color"               , "Always enables color",                   { color: true }],
                 ["--no-color"            , "Always disables color",                  { color: false }]
+            ]],
+            ["Configuration", [
+                ["-t", "--timeout"       , "Configure connection timeout time. For " +
+                                           "ICP connections you might want to change " +
+                                           "this as latency can be higher.",         { arg: 'timeout', timeout: true }],
+                ["-T", "--no-timeout"    , "Disables timeout. Doesn't have any major " +
+                                           "downsides if running locally",           { arg: 'timeout', timeout: false }]
             ]]
         ]);
     }
@@ -41,7 +51,8 @@ export default class ParserServer extends CompilerCLI {
         const colorValue = {
             'status': 32,
             'warn': 33,
-            'error': 31
+            'error': 31,
+            'conf': 36
         }[type] || '32';
         const color = this.color ? `\u001B[1;${colorValue}m` : ``;
         const endColor = this.color ? `\u001B[0m` : ``;
@@ -53,12 +64,14 @@ export default class ParserServer extends CompilerCLI {
 
     run(args) {
         if (cluster.isWorker) {
+            this.color = process.env[COLOR_KEY] === 'true';
             this.startWorker(process.env[SERVER_PORT_KEY]);
             return;
         }
 
         let port = null;
-        let color = tty.isatty(1);
+        this.color = tty.isatty(1);
+        let timeout = DEFAULT_TIMEOUT; // defualt timeout
 
         for (let i = 0; i < args.length; i++) {
             if (args[i][0] === "-" && args[i].length > 1) {
@@ -74,15 +87,29 @@ export default class ParserServer extends CompilerCLI {
                     outputDirectory = args[++i];
                 }
 
-                if ('color' in flagInfo) color = args[++i];
-            } else if (!isNaN(+args[i])) {
-                port = +args[i];
+                if ('color' in flagInfo) this.color = !!flagInfo.color;
+                if ('timeout' in flagInfo) {
+                    if (flagInfo.timeout) {
+                        timeout = args[++i] |0;
+                        if (isNaN(timeout)) this.error.cli(`timeout must be integer`);
+                        this.printLog(`Timeout set to ${timeout}ms`, 'conf');
+                    } else {
+                        this.printLog('Disabling timeout', 'conf');
+                        timeout = -1;
+                    }
+                }
+            } else if (port === null) {
+                if (!isNaN(+args[i])) {
+                    port = +args[i];
+                } else {
+                    port = args[i];
+                }
             } else {
                 this.error.cli(`unknown parameter: ${args[i]}`);
             }
         }
 
-        this.color = color;
+        this.timeout = timeout;
 
         this.startServer(port || DEFAULT_PORT);
     }
@@ -93,6 +120,7 @@ export default class ParserServer extends CompilerCLI {
      */
     startWorker(port) {
         const clusterId = cluster.worker.id;
+        const timeout = +process.env[SERVER_TIMEOUT_KEY];
         this.printLog(`Starting worker ${clusterId}`, 'status');
 
         let connectionCounter = 0;
@@ -104,14 +132,19 @@ export default class ParserServer extends CompilerCLI {
             const connectionIndex = connectionCounter++;
             const instance = `${clusterId}.${connectionIndex}`;
 
-            this.printLog(`Instance ${instance} received`, 'status');
+            this.printLog(`Instance ${instance} started; listening.`, 'status');
 
+            let startTime = process.hrtime(),
+                dataEndTime,
+                parserEndTime;
 
             // Timeout if client abruptly stops sending
-            c.setTimeout(300, () => {
-                this.printLog(`Socket (${instance}) timeout`, 'warn');
-                c.end();
-            });
+            if (timeout !== -1) {
+                c.setTimeout(timeout, () => {
+                    this.printLog(`Socket (${instance}) timeout.`, 'warn');
+                    c.end();
+                });
+            }
 
 
             // Decompress data as it comes in
@@ -125,12 +158,20 @@ export default class ParserServer extends CompilerCLI {
 
             // When all done
             c.on('close', () => {
-                this.printLog(`Instance ${instance} completed`, 'status');
+                const timeFormat = parserEndTime ? ` (${parserEndTime}ms)` : '';
+                this.printLog(`Instance ${instance} completed${timeFormat}`, 'status');
             });
+
+            c.on('error', (error) => {
+                this.printLog(`Uncaught error at instance ${instance}.\n${error.stack}`, 'error');
+            })
 
             // When all data is received
             c.on('end', () => {
-                this.printLog(`Instance ${instance} processed ${data.length} bytes`, 'status');
+                const timeTookToReceiveData = process.hrtime(startTime);
+                dataEndTime = ((timeTookToReceiveData[0] + timeTookToReceiveData[1] / 1e9) * 1e3).toFixed(4);
+
+                this.printLog(`Instance ${instance} processed ${data.length} bytes (${dataEndTime}ms)`, 'status');
 
                 const parser = new VSLParser();
 
@@ -154,6 +195,8 @@ export default class ParserServer extends CompilerCLI {
                         const serializer = new ASTSerializer(results[0]);
                         serializer.serializeTo(c)
                             .then(() => {
+                                const timeTookToParse = process.hrtime(startTime);
+                                parserEndTime = ((timeTookToParse[0] + timeTookToParse[1] / 1e9) * 1e3 - +dataEndTime).toFixed(4);
                                 // Close TCP when finished
                                 c.end();
                             })
@@ -170,11 +213,15 @@ export default class ParserServer extends CompilerCLI {
         server.listen(port, () => {
             const address = server.address();
             const location = typeof address === 'string' ? `ICP ${address}` : `TCP [${address.address}]:${address.port} over ${address.family}`;
-            this.printLog(`Worker ${cluster.worker.id} at ${location}`, 'status');
+            this.printLog(`Worker ${cluster.worker.id} at ${location}`, 'conf');
         });
 
         server.on('close', () => {
             this.printLog(`Closing server.`, 'status');
+        });
+
+        server.on('error', (e) => {
+            this.printLog(`Uncaught server error.\n${e}`, 'error');
         });
     }
 
@@ -183,7 +230,8 @@ export default class ParserServer extends CompilerCLI {
      * @param {number} port
      */
     startServer(port) {
-        this.printLog(`Starting on port ${port}`, 'status');
+        const locationType = isNaN(+port) ? 'path' : 'port';
+        this.printLog(`Starting on ${locationType} ${port}`, 'status');
 
         const workerCount = os.cpus().length;
 
@@ -191,7 +239,9 @@ export default class ParserServer extends CompilerCLI {
 
         for (let i = 0; i < workerCount; i++) {
             const worker = cluster.fork({
-                [SERVER_PORT_KEY]: port
+                [SERVER_PORT_KEY]: port,
+                [SERVER_TIMEOUT_KEY]: this.timeout,
+                [COLOR_KEY]: this.color
             });
         }
 
